f26c19f65aff78825d799e7f5822dd4fd5e9acd2
geralexcas <geralexcas@gmail.com>
2026-04-28 17:16:25 -0500

    configurando para que el codigo sea dinamico en api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.VUE_APP_API_URL || 'http://localhost:8001',
+  baseURL: process.env.VUE_APP_API_URL || `${window.location.protocol}//${window.location.hostname}:8001`,
   headers: {
     'Content-Type': 'application/json'
   }
